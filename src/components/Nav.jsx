c4552d84a238bbc20e73623abb4ae5f8e6716cef
Lorea De La Torre <loreataina@icloud.com>
2025-07-20 08:44:22 -0500

    Refactor Nav component to improve menu functionality and structure

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -3,12 +3,20 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import LibraryLogo from "../assets/Library.svg";
 
 function Nav() {
+  function openMenu() {
+    document.body.classList.add("menu--open");
+  }
+
+  function closeMenu() {
+    document.body.classList.remove("menu--open");
+  }
+
   return (
     <div className="nav__container">
       <a href="/">
         <img src={LibraryLogo} alt="" className="logo" />
       </a>
-      <ul className="nav__links"> 
+      <ul className="nav__links">
         <li className="nav__list">
           <a href="/" className="nav__link">
             Home
@@ -19,35 +27,35 @@ function Nav() {
             Books
           </a>
         </li>
-        <button className="btn__menu">
+        <button className="btn__menu" onClick={openMenu}>
           <FontAwesomeIcon icon="fa-solid fa-bars" />
         </button>
-      <li className="nav__icon">
-        <a href="/cart" className="nav__link">
-          <FontAwesomeIcon icon="fa-solid fa-cart-shopping" />
-        </a>
-      </li>
+        <li className="nav__icon">
+          <a href="/cart" className="nav__link">
+            <FontAwesomeIcon icon="fa-solid fa-cart-shopping" />
+          </a>
+        </li>
       </ul>
       <div className="menu__backdrop">
-        <button className="btn__menu btn__menu--close">
-            <FontAwesomeIcon icon="fa-solid fa-xmark" />
+        <button className="btn__menu btn__menu--close" onClick={closeMenu}>
+          <FontAwesomeIcon icon="fa-solid fa-xmark" />
         </button>
         <ul className="menu__links">
-            <li className="menu__list">
-                <a href="/" className="menu__link">
-                 Home   
-                </a>
-            </li>
-            <li className="menu__list">
-                <a href="/books" className="menu__link">
-                    Books
-                </a>
-            </li>
-            <li className="menu__list">
-                <a href="/cart" className="menu__link">
-                    Cart
-                </a>
-            </li>
+          <li className="menu__list">
+            <a href="/" className="menu__link">
+              Home
+            </a>
+          </li>
+          <li className="menu__list">
+            <a href="/books" className="menu__link">
+              Books
+            </a>
+          </li>
+          <li className="menu__list">
+            <a href="/cart" className="menu__link">
+              Cart
+            </a>
+          </li>
         </ul>
       </div>
     </div>
